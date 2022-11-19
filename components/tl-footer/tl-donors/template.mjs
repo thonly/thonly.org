c2c209379e9e50ddd7384e1acd37d9637627b033
Thon Ly <thonly@heartbank.ceo>
2022-11-18 18:23:33 -0800

    refactor

--- components/tl-footer/tl-donors/template.mjs
+++ components/tl-footer/tl-donors/template.mjs
@@ -1,9 +1,9 @@
-import { THONLY } from "/global.mjs";
+import { ORIGIN } from "/global.mjs";
 const template = document.createElement("template");
 
 template.innerHTML = `
-    <link rel="stylesheet" href="${THONLY}/components/tl-footer/shadow.css">
-    <link rel="stylesheet" href="${THONLY}/components/tl-footer/tl-donors/shadow.css">
+    <link rel="stylesheet" href="${ORIGIN}/components/tl-footer/shadow.css">
+    <link rel="stylesheet" href="${ORIGIN}/components/tl-footer/tl-donors/shadow.css">
     <h5>Donors</h5>
     <ul class="donors">
         <li><a href="https://github.com/panhiathao"><img src="https://github.com/panhiathao.png?size=100"></a></li>
