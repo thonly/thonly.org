bb5a07bfbe62f0eca336498f860e65acdda469c7
Thon Ly <thonly@heartbank.ceo>
2023-01-05 20:10:26 -0800

    refactor

--- global.mjs
+++ global.mjs
@@ -34,9 +34,9 @@ export const TRILOGY = (() => {
 })();
 
 export const GAME = (() => {
-    if (window.location.port === "5651" || window.location.hostname === "quiz.siliconwat.com") return ["quiz", "Programming Concept Quizzes", "radial-gradient(circle at bottom left, #773344 0%, #E3B5A4 100%)"];
-    if (window.location.port === "5652" || window.location.hostname === "code.siliconwat.com") return ["code", "Interactive Coding Exercises", "radial-gradient(circle at bottom left, #DBCBD8 0%, #564787 100%)"];
-    if (window.location.port === "5653" || window.location.hostname === "flashcard.siliconwat.com") return ["flashcard", "Syntax Flashcard Games", "radial-gradient(circle at bottom left, #9AD4D6 0%, #101935 100%)"];
+    if (window.location.port === "5651" || window.location.hostname === "quiz.siliconwat.com") return ["quiz", window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5651" : "https://quiz.siliconwat.com", "radial-gradient(circle at bottom left, #773344 0%, #E3B5A4 100%)"];
+    if (window.location.port === "5652" || window.location.hostname === "code.siliconwat.com") return ["code", window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5652" : "https://code.siliconwat.com", "radial-gradient(circle at bottom left, #DBCBD8 0%, #564787 100%)"];
+    if (window.location.port === "5653" || window.location.hostname === "flashcard.siliconwat.com") return ["flashcard", window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5653" : "https://flashcard.siliconwat.com", "radial-gradient(circle at bottom left, #9AD4D6 0%, #101935 100%)"];
     return [null, null, null];
 })();
 
