ca03b60429753d03728eb9db2ed393a271459af8
Thon Ly <thonly@heartbank.ceo>
2023-02-11 05:23:10 -0800

    refactor header css

--- components/tl-header/template.mjs
+++ components/tl-header/template.mjs
@@ -29,89 +29,91 @@ template.innerHTML = `
         <label class='menu-button-container' for="menu-toggle">
             <span class='menu-button'></span>
         </label>
-        <ul>
-            <li>
-                <h3>Research</h3>
-                <nav>
-                    <a href="https://333.eco"><button>333.eco</button></a>
-                    <h4>Research & Development</h4>
-                    <menu>
-                        <!--<li><a>Micro Wallet</a></li>
-                        <li><a>Metaverse Gear</a></li>-->
-                        <li><a href="#internet">Free Internet</a></li>
-                        <li><a href="#energy">Free Energy</a></li>
-                        <li><a href="#physics">Zero-Point Physics</a></li>
-                    </menu>
-                    <h4>Doctoral Dissertation</h4>
-                    <menu>
-                        <li><a href="#ai">Artificial Intelligence</a></li>
-                        <li><a href="#qi">Quantum Intelligence</a></li>
-                        <li><a href="#ei">Enlightened Intelligence</a></li>
-                    </menu>
-                </nav>
-            </li>
-            <li>
-                <h3>Work</h3>
-                <nav>
-                    <a href="https://me.thonly.org"><button>Business Card</button></a>
-                    <h4>Instructor at Silicon Wat</h4>
-                    <menu>
-                        <li><a href="#university">University</a></li>
-                        <li><a href="#campus">Campus</a></li>
-                        <li><a href="#agency">Agency</a></li>
-                    </menu>
-                    <h4>Game Creator at THonly™</h4>
-                    <menu>
-                        <li><a href="#studios">Studios</a></li>
-                        <li><a href="#metaverse">Metaverse</a></li>
-                    </menu>
-                    <h4>Organic Farmer at Nhia Kou</h4>
-                    <menu>
-                        <li><a href="#markets">Markets</a></li>
-                        <li><a href="#orchards">Orchards</a></li>
-                    </menu>
-                    <h4>Founder at HeartBank<sup>®</sup></h4>
-                    <menu>
-                        <li><a href="#heartbank">Subsidiaries</a></li>
-                        <li><a href="#foundation">Foundation</a></li>
-                    </menu>
-                </nav>
-            </li>
-            <li>
-                <h3>Philanthropy</h3>
-                <nav>
-                    <a href="https://kiitos.earth"><button>Kiitos.Earth</button></a>
-                    <h4>Dear Kiitos</h4>
-                    <menu>
-                        <li><a href="#equation">Zero Hero Equation</a></li>
-                        <li><a href="#blockchain">Non Zero-Sum Blockchain</a></li>
-                    </menu>
-                    <h4>Kitty Kiitos</h4>
-                    <menu>
-                        <li><a href="#sports">Sports</a></li>
-                        <li><a href="#games">Metaverse</a></li>
-                    </menu>
-                </nav>
-            </li>
-            <li>
-                <h3>Meditation</h3>
-                <nav>
-                    <a href="https://zero.thonly.org"><button>Zero Joy Love</button></a>
-                    <h4>Music</h4>
-                    <menu>
-                        <li><a href="#podcast">The Code of Success Podcast</a></li>
-                        <li><a href="#radio">Coding Music Radio Station</a></li>
-                        <li><a href="#music">Sinn Sisamouth Music Library</a></li>
-                    </menu>
-                    <h4>Sports</h4>
-                    <menu>
-                        <li><a href="#kendo">Kendo</a></li>
-                        <li><a href="#takraw">Sepak Takraw</a></li>
-                        <li><a href="#skating">Ice Skating</a></li>
-                    </menu>
-                </nav>
-            </li>
-        </ul>
+        <div>
+            <ul>
+                <li>
+                    <h3>Research</h3>
+                    <nav>
+                        <a href="https://333.eco"><button>333.eco</button></a>
+                        <h4>Research & Development</h4>
+                        <menu>
+                            <!--<li><a>Micro Wallet</a></li>
+                            <li><a>Metaverse Gear</a></li>-->
+                            <li><a href="#internet">Free Internet</a></li>
+                            <li><a href="#energy">Free Energy</a></li>
+                            <li><a href="#physics">Zero-Point Physics</a></li>
+                        </menu>
+                        <h4>Doctoral Dissertation</h4>
+                        <menu>
+                            <li><a href="#ai">Artificial Intelligence</a></li>
+                            <li><a href="#qi">Quantum Intelligence</a></li>
+                            <li><a href="#ei">Enlightened Intelligence</a></li>
+                        </menu>
+                    </nav>
+                </li>
+                <li>
+                    <h3>Work</h3>
+                    <nav>
+                        <a href="https://me.thonly.org"><button>Business Card</button></a>
+                        <h4>Instructor at Silicon Wat</h4>
+                        <menu>
+                            <li><a href="#university">University</a></li>
+                            <li><a href="#campus">Campus</a></li>
+                            <li><a href="#agency">Agency</a></li>
+                        </menu>
+                        <h4>Game Creator at THonly™</h4>
+                        <menu>
+                            <li><a href="#studios">Studios</a></li>
+                            <li><a href="#metaverse">Metaverse</a></li>
+                        </menu>
+                        <h4>Organic Farmer at Nhia Kou</h4>
+                        <menu>
+                            <li><a href="#markets">Markets</a></li>
+                            <li><a href="#orchards">Orchards</a></li>
+                        </menu>
+                        <h4>Founder at HeartBank<sup>®</sup></h4>
+                        <menu>
+                            <li><a href="#heartbank">Subsidiaries</a></li>
+                            <li><a href="#foundation">Foundation</a></li>
+                        </menu>
+                    </nav>
+                </li>
+                <li>
+                    <h3>Philanthropy</h3>
+                    <nav>
+                        <a href="https://kiitos.earth"><button>Kiitos.Earth</button></a>
+                        <h4>Dear Kiitos</h4>
+                        <menu>
+                            <li><a href="#equation">Zero Hero Equation</a></li>
+                            <li><a href="#blockchain">Non Zero-Sum Blockchain</a></li>
+                        </menu>
+                        <h4>Kitty Kiitos</h4>
+                        <menu>
+                            <li><a href="#sports">Sports</a></li>
+                            <li><a href="#games">Metaverse</a></li>
+                        </menu>
+                    </nav>
+                </li>
+                <li>
+                    <h3>Meditation</h3>
+                    <nav>
+                        <a href="https://zero.thonly.org"><button>Zero Joy Love</button></a>
+                        <h4>Music</h4>
+                        <menu>
+                            <li><a href="#podcast">The Code of Success Podcast</a></li>
+                            <li><a href="#radio">Coding Music Radio Station</a></li>
+                            <li><a href="#music">Sinn Sisamouth Music Library</a></li>
+                        </menu>
+                        <h4>Sports</h4>
+                        <menu>
+                            <li><a href="#kendo">Kendo</a></li>
+                            <li><a href="#takraw">Sepak Takraw</a></li>
+                            <li><a href="#skating">Ice Skating</a></li>
+                        </menu>
+                    </nav>
+                </li>
+            </ul>
+        </div>
         <aside>
             <select onchange="this.getRootNode().host.changeLanguage(event)">
                 <option value="en">English</option>
